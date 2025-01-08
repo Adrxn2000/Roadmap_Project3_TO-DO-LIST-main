e39d077e6b573b65c311091e1c06107e1033d739
Adrxn2000 <amajavu@dvtsoftware.com>
2025-01-08 14:35:56 +0200

    new commit

--- Roadmap_Project3_TO-DO-LIST-main/script.js
+++ Roadmap_Project3_TO-DO-LIST-main/script.js
@@ -1,6 +1,31 @@
-const inputBox = document.getElementById("input-box");
-const addBtn = document.getElementById("add-btn");
+//Selecting DOM elements
+const taskInput = document.getElementById("task");
+const taskList = document.getElementById("task-list");
+const searchInput = document.getElementById("search");
 
+//localStorage
+const loadTasks = () => JSON.parse(localStorage.getItem("tasks")) || [];
+const saveTasks = (tasks) => localStorage.setItem("tasks", JSON.stringify(tasks));
+
+let tasks = loadTasks();
+
+// Rendering tasks list
+const renderTasks = (filter = "") => {  
+    taskList.innerHTML = "";
+    const filteredTasks = tasks.filter((task) => task.toLowerCase().includes(filter.toLowerCase()));
+    filteredTasks.forEach((task, index) => {
+        const taskItem = document.createElement("li");
+        listItem.className = "task-item";
+
+        const textSpan = document.createElement("span");
+        textSpan.textContent = task.text;
+        textSpan.contentEditable = false;
+
+
+
+        taskItem.innerText = task;
+        taskList.appendChild(taskItem);
+    });
 function addTask() {
   const task = inputBox.value;
   if (task) {
@@ -10,4 +35,5 @@ function addTask() {
     taskList.appendChild(taskItem);
     inputBox.value = "";
   }
+}
 }
